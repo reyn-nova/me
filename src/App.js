17dd3bbee80fe0cd84c893faa19cc79ae0704d6f
rp-nova <reynaldpn@gmail.com>
2020-05-26 08:54:48 +0700

    initial project setup

--- src/App.js
+++ src/App.js
@@ -1,26 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+export default class extends Component {
+  render() {
+    return (
+      <div
+        className = 'container'
+      >
+        <h1
+          className = 'welcome-text'
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+          Hi there, welcome to my site
+        </h1>
+      </div>
+    )
+  }
 }
-
-export default App;
